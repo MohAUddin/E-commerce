251b15d4133eb1c6e2f99d2ed5588aaa475ef1d6
MohAUddin <mohammad.uddin454@gmail.com>
2022-09-09 23:33:21 -0400

    added rating sort filter

--- books.js
+++ books.js
@@ -5,11 +5,11 @@ function renderBooks(filter) {
 
   if (filter === 'LOW_TO_HIGH'){
     books.sort((a, b) => a.originalPrice - b.originalPrice);
-  }
-  else if (filter === 'HIGH_TO_LOW') {
+  }else if (filter === 'HIGH_TO_LOW') {
     books.sort((a, b) => b.originalPrice - a.originalPrice);
+  }else if (filter === "RATING"){
+    books.sort((a, b) => b.rating - a.rating);
   }
-
 
   const booksHtml = books
     .map((book) => {
